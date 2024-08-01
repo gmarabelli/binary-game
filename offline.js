8d0645d55db9cad5d22aad249812a1be506cefb6
gmarabelli <gmarabelli@itiscardanopv.edu.it>
2024-08-01 14:09:42 +0200

    edited service worker

--- offline.js
+++ offline.js
@@ -1,15 +1,19 @@
 const CACHE_NAME = "binary-game";
 
 self.addEventListener("install", async (event) => {
-	const cache = await caches.open(CACHE_NAME);
-	event.waitUntil(await cache.addAll([
-		"binary-game.html",
-		"style.css",
-		"script.js",
-	]));
+	event.waitUntil(() => {
+		const cache = await caches.open(CACHE_NAME);
+		await cache.addAll([
+			"binary-game.html",
+			"style.css",
+			"script.js",
+		]);
+	}());
 });
 
 self.addEventListener("fetch", async (event) => {
-	const cache = await caches.open(CACHE_NAME);
-	event.respondWith(cache.match(event.request));
+	event.respondWith(() => {
+		const cache = await caches.open(CACHE_NAME);
+		return cache.match(event.request);
+	}());
 });

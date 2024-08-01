bc3a0ff6a9aac6b6e1ab2441f160bbe414c8c8ef
gmarabelli <gmarabelli@itiscardanopv.edu.it>
2024-08-01 14:13:32 +0200

    added brackets to service worker anonymous functions

--- offline.js
+++ offline.js
@@ -1,19 +1,19 @@
 const CACHE_NAME = "binary-game";
 
 self.addEventListener("install", async (event) => {
-	event.waitUntil(async () => {
+	event.waitUntil((async () => {
 		const cache = await caches.open(CACHE_NAME);
 		await cache.addAll([
 			"binary-game.html",
 			"style.css",
 			"script.js",
 		]);
-	}());
+	})());
 });
 
 self.addEventListener("fetch", async (event) => {
-	event.respondWith(async () => {
+	event.respondWith((async () => {
 		const cache = await caches.open(CACHE_NAME);
 		return cache.match(event.request);
-	}());
+	})());
 });
